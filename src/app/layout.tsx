d67803381c5e3b065a572e0759b7e382514cff53
samsites2000 <sam@samsites.com>
2026-02-11 19:01:53 +0000

    Test: Disable ScrollRevealAnimations too - testing with NO animations at all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -251,7 +251,7 @@ export default function RootLayout({
               <VideoModalProvider>
                 {/* <Preloader /> */}
                 {/* <PageTransition /> */}
-                <ScrollRevealAnimations />
+                {/* <ScrollRevealAnimations /> */}
                 {/* <CustomCursor /> */}
                 <VideoModal />
                 <Navbar />
